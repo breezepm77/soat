fb0d8947b445cda763c25e17810148e032bd8ebc
rofiqusmonov <87475791+rofiqusmonov@users.noreply.github.com>
2021-09-18 17:56:50 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function timeShower() {
     var timeNow = new Date();
 
-document.querySelector(".hours").textContent = timeNow.getHours().padStart(2,0);
-document.querySelector(".minutes").textContent = timeNow.getMinutes().padStart(2,0);
-document.querySelector(".seconds").textContent = timeNow.getSeconds().padStart(2,0);
+document.querySelector(".hours").textContent = String(timeNow.getHours()).padStart(2,0);
+document.querySelector(".minutes").textContent = String(timeNow.getMinutes()).padStart(2,0);
+document.querySelector(".seconds").textContent = String(timeNow.getSeconds()).padStart(2,0);
 }
 
 timeShower();
